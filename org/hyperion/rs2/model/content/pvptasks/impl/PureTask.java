971b05d475ad173776c6b0cd427f16b7586dc31e
fuzenseth <paras-best@hotmail.com>
2014-10-31 21:55:19 +0200

    was already there :)

--- org/hyperion/rs2/model/content/pvptasks/impl/PureTask.java
+++ org/hyperion/rs2/model/content/pvptasks/impl/PureTask.java
@@ -7,7 +7,7 @@ import org.hyperion.rs2.model.content.pvptasks.PvPTask;
 import org.hyperion.rs2.model.newcombat.Skills;
 
 public class PureTask extends PvPTask {
-
+    //not ready yet :p and no sql is not enabled
 	@Override
 	public boolean isTask(Player p, Player o) {
 		return p.getPvPTask() != null && p.getPvPTask() instanceof PureTask &&

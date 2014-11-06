5fad31005a2055c425ecac888d7a259205c5e699
dvpkjoshy <joshua.maione@hotmail.com>
2014-11-06 12:06:16 -0500

    different behaviour for check punishment: if the player is online -> check punishments for that ip and mac as well

--- org/hyperion/rs2/model/punishment/cmd/CheckPunishmentCommand.java
+++ org/hyperion/rs2/model/punishment/cmd/CheckPunishmentCommand.java
@@ -2,34 +2,33 @@ package org.hyperion.rs2.model.punishment.cmd;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import org.hyperion.rs2.commands.Command;
 import org.hyperion.rs2.model.Player;
 import org.hyperion.rs2.model.Rank;
+import org.hyperion.rs2.model.World;
 import org.hyperion.rs2.model.punishment.Punishment;
 import org.hyperion.rs2.model.punishment.holder.PunishmentHolder;
 import org.hyperion.rs2.model.punishment.manager.PunishmentManager;
 
 public class CheckPunishmentCommand extends Command {
 
-    private static final String IP_REGEX = "\\d+\\.\\d+\\.\\d+\\.\\d+";
-
     public CheckPunishmentCommand(){
         super("checkpunish", Rank.HELPER);
     }
 
     public boolean execute(final Player player, final String input){
-        final String target = filterInput(input);
+        final String targetName = filterInput(input);
+        final Player target = World.getWorld().getPlayer(targetName);
         final List<Punishment> punishments = new ArrayList<>();
-        if(target.matches(IP_REGEX) && Rank.hasAbility(player, Rank.ADMINISTRATOR)){
-            punishments.addAll(PunishmentManager.getInstance().getByIp(target));
-        }else{
-            final PunishmentHolder holder = PunishmentManager.getInstance().get(target);
-            if(holder != null)
-                punishments.addAll(holder.getPunishments());
-        }
-        if(punishments.isEmpty()){
-            player.sendf("No punishments found for: %s", target);
-            return false;
+        final PunishmentHolder holder = PunishmentManager.getInstance().get(targetName);
+        if(holder != null)
+            punishments.addAll(holder.getPunishments());
+        if(target != null){
+            for(final PunishmentHolder h : PunishmentManager.getInstance().getHolders())
+                for(final Punishment p : h.getPunishments())
+                    if(Objects.equals(target.getShortIP(), p.getVictimIp()) || target.getUID() == p.getVictimMac())
+                        punishments.add(p);
         }
         for(final Punishment p : punishments)
             p.send(player, false);

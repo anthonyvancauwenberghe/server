afc58fb6e3866959a37856c8d044d869aee82394
dvpkjoshy <joshua.maione@hotmail.com>
2014-11-06 12:10:10 -0500

    different behaviour for check punishment: if the player is online -> check punishments for that ip and mac as well

--- org/hyperion/rs2/model/punishment/cmd/CheckPunishmentCommand.java
+++ org/hyperion/rs2/model/punishment/cmd/CheckPunishmentCommand.java
@@ -21,14 +21,17 @@ public class CheckPunishmentCommand extends Command {
         final String targetName = filterInput(input);
         final Player target = World.getWorld().getPlayer(targetName);
         final List<Punishment> punishments = new ArrayList<>();
-        final PunishmentHolder holder = PunishmentManager.getInstance().get(targetName);
-        if(holder != null)
-            punishments.addAll(holder.getPunishments());
-        if(target != null){
-            for(final PunishmentHolder h : PunishmentManager.getInstance().getHolders())
-                for(final Punishment p : h.getPunishments())
-                    if(Objects.equals(target.getShortIP(), p.getVictimIp()) || target.getUID() == p.getVictimMac())
-                        punishments.add(p);
+        for(final PunishmentHolder h : PunishmentManager.getInstance().getHolders()){
+            for(final Punishment p : h.getPunishments()){
+                if(p.getVictimName().equalsIgnoreCase(targetName)){
+                    punishments.add(p);
+                    continue;
+                }
+                if(target == null)
+                    continue;
+                if(Objects.equals(target.getShortIP(), p.getVictimIp()) || target.getUID() == p.getVictimMac())
+                    punishments.add(p);
+            }
         }
         for(final Punishment p : punishments)
             p.send(player, false);

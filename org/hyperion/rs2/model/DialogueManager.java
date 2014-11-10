e41cd6c93758a3a5cfc45adb6095bf9d4030ac29
dvpkjoshy <joshua.maione@hotmail.com>
2014-11-09 23:15:28 -0500

    display name

--- org/hyperion/rs2/model/DialogueManager.java
+++ org/hyperion/rs2/model/DialogueManager.java
@@ -861,21 +861,21 @@ public class DialogueManager {
 				if(ModerationOverride.canModerate(player))
 					ModerationOverride.kickPlayer(player.getModeration());
 				player.getActionSender().removeAllInterfaces();
-				PushMessage.pushGlobalMessage(player.getName() + " has just kicked " + player.getModeration().getSafeDisplayName() + ".");
+				PushMessage.pushGlobalMessage(player.getSafeDisplayName() + " has just kicked " + player.getModeration().getSafeDisplayName() + ".");
 				player.setModeration(null);
 				break;
 			case 138:
 				if(ModerationOverride.canModerate(player))
 					ModerationOverride.jailPlayer(player.getModeration());
 				player.getActionSender().removeAllInterfaces();
-				PushMessage.pushGlobalMessage(player.getName() + " has just jailed " + player.getModeration().getSafeDisplayName() + ".");
+				PushMessage.pushGlobalMessage(player.getSafeDisplayName() + " has just jailed " + player.getModeration().getSafeDisplayName() + ".");
 				player.setModeration(null);
 				break;
 			case 139:
 				if(ModerationOverride.canModerate(player))
 					ModerationOverride.mutePlayer(player.getModeration());
 				player.getActionSender().removeAllInterfaces();
-				PushMessage.pushGlobalMessage(player.getName() + " has just muted " + player.getModeration().getSafeDisplayName() + ".");
+				PushMessage.pushGlobalMessage(player.getSafeDisplayName() + " has just muted " + player.getModeration().getSafeDisplayName() + ".");
 				player.setModeration(null);
 				break;
 			case 140:

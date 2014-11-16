ef9c7d7d62ad37c7266d0733c707c17b318678c1
Alsk Jet <swm321@gmail.com>
2014-11-16 09:14:14 -0500

    JANK

--- org/hyperion/rs2/model/container/EquipmentReq.java
+++ org/hyperion/rs2/model/container/EquipmentReq.java
@@ -95,10 +95,10 @@ public class EquipmentReq {
 		if(itemLevels.get(Integer.valueOf(id)) != null) {
 			abyte0 = (byte[]) itemLevels.get(Integer.valueOf(id));
 		}
-		if(abyte0 == null) {
+		//if(abyte0 == null) {
 			abyte0 = getRequirements(ItemDefinition.forId(id).getName(), id);
 			itemLevels.put(Integer.valueOf(id), abyte0);
-		}
+		//}
 		for(int j = 0; j < 21; j++) {
 			if(player.getSkills().getLevelForExp(j) < abyte0[j]) {
 				player.getSkills();

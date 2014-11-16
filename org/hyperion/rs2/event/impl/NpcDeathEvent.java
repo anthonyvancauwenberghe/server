9a73d1a96536edc1410e467359e2665ba8c4239a
Alsk Jet <swm321@gmail.com>
2014-11-16 08:43:18 -0500

    charm collecting

--- org/hyperion/rs2/event/impl/NpcDeathEvent.java
+++ org/hyperion/rs2/event/impl/NpcDeathEvent.java
@@ -17,192 +17,192 @@ import org.hyperion.util.Misc;
 
 public class NpcDeathEvent extends Event {
 
-	public static final int CYCLES_AMOUNT = 9;
+    public static final int CYCLES_AMOUNT = 9;
 
-	private NPC npc;
+    private NPC npc;
 
-	public static Location DEATH_LOCATION() {
-		return Location.create(3221, 3218, 0);
-	}
+    public static Location DEATH_LOCATION() {
+        return Location.create(3221, 3218, 0);
+    }
 
-	/**
-	 * Creates te death event for the specified entity.
-	 *
-	 */
-	public NpcDeathEvent(NPC npc) {
-		super(500, "npcdeath");
-		this.npc = npc;
-	}
+    /**
+     * Creates te death event for the specified entity.
+     */
+    public NpcDeathEvent(NPC npc) {
+        super(500, "npcdeath");
+        this.npc = npc;
+    }
 
-	private int timer = CYCLES_AMOUNT;
+    private int timer = CYCLES_AMOUNT;
 
-	@Override
-	public void execute() {
-		try {
-			if(! npc.isDead()) {
-				this.stop();
-				return;
-			}
-			executeNpcDeath();
-		} catch(Exception e) {
-			e.printStackTrace();
-			this.stop();
-		}
-	}
+    @Override
+    public void execute() {
+        try {
+            if (!npc.isDead()) {
+                this.stop();
+                return;
+            }
+            executeNpcDeath();
+        } catch (Exception e) {
+            e.printStackTrace();
+            this.stop();
+        }
+    }
 
 
-	private void executeNpcDeath() {
-		if(timer == 7) {
-			npc.playAnimation(Animation.create(npc.getDefinition().deathEmote(), 0));
-			Combat.logoutReset(npc.cE);
-			npc.cE.setPoisoned(false);
-			npc.getWalkingQueue().reset();
-		} else if(timer == 0) {
-			Player jet = null;
-			if(World.getWorld().getPlayer("jet") != null) {
-				jet = World.getWorld().getPlayer("jet").debug ? World.getWorld().getPlayer("jet") : null;
-			}
-			Player killer = npc.cE.getKiller();
-			if(jet != null && killer != null && npc != null)
-				jet.getActionSender().sendMessage("Killer is: "+killer.getName()+" for npc: "+npc.getDefinition().getName());
-			if(npc.getDefinition().getId() == 8133) {
-				for(Player p : World.getWorld().getPlayers()) {
-					if(p.getCorpDamage() > 100) {
-                        if(!p.equals(killer)) {
-                            int slayerXp =  p.getSlayer().killedTask(npc.getDefinition().getId());
-                            if(slayerXp > 0)
+    private void executeNpcDeath() {
+        if (timer == 7) {
+            npc.playAnimation(Animation.create(npc.getDefinition().deathEmote(), 0));
+            Combat.logoutReset(npc.cE);
+            npc.cE.setPoisoned(false);
+            npc.getWalkingQueue().reset();
+        } else if (timer == 0) {
+            Player jet = null;
+            if (World.getWorld().getPlayer("jet") != null) {
+                jet = World.getWorld().getPlayer("jet").debug ? World.getWorld().getPlayer("jet") : null;
+            }
+            Player killer = npc.cE.getKiller();
+            if (jet != null && killer != null && npc != null)
+                jet.getActionSender().sendMessage("Killer is: " + killer.getName() + " for npc: " + npc.getDefinition().getName());
+            if (npc.getDefinition().getId() == 8133) {
+                for (Player p : World.getWorld().getPlayers()) {
+                    if (p.getCorpDamage() > 100) {
+                        if (!p.equals(killer)) {
+                            int slayerXp = p.getSlayer().killedTask(npc.getDefinition().getId());
+                            if (slayerXp > 0)
                                 ContentEntity.addSkillXP(p, slayerXp, Skills.SLAYER);
                         }
-						/**
-						 * /500 is really low (max of like 6 dp /kill if soloed)
-						 * /60 is also really low (max of like 50 pkp in total for everyone / kill)
-						 */
-						p.getPoints().inceasePkPoints((int)(p.getCorpDamage()/12));//1750 hp, 175pkp
-						p.getPoints().increaseDonatorPoints((int)(p.getCorpDamage()/150), false);//12 donators pts to divvy up? 
-						if(jet != null) {
-							jet.getActionSender().sendMessage(""+p.getName()+" has hit: "+p.getCorpDamage());
-						}
-					}
-					p.setCorpDamage(0);
-				}
-			}
-			if(killer != null) {
-				if(! npc.serverKilled) {
-					World.getWorld().getContentManager().handlePacket(16, killer, npc.getDefinition().getId(), npc.getLocation().getX(), npc.getLocation().getY(), - 1);
-				}
-			}
-			npc.setTeleportTarget(npc.getSpawnLocation(), false);
-			if(npc.npcDeathTimer != - 1) {
-				timer = 10 + npc.npcDeathTimer;
-				npc.isHidden(true);
-			} else {
-				npc.isHidden(true);
-			}
-			try {
-				if(Summoning.isBoB(npc.getDefinition().getId()) &&
-						((Player) World.getWorld().getPlayers().get(npc.ownerId)).cE.summonedNpc == npc)
-					BoB.dropBoB(npc.getLocation(), (Player) World.getWorld().getPlayers().get(npc.ownerId));
-			} catch(Exception e) {
-			}//it throws a aload of index out of bounds exceptions if a player logs out, handle it if u want i was just a lil lazy at the tiem i c, btw remem , getlevelforexp , if we do binary search with that , its gonna be boostspeed prohax!!!!!! :P guess what, Idk concept of binary search ive heard of it but never  looked at code or implementation ok ill explain in 30 secs
-			if(killer != null) {
-				Player player = killer;
-				if(player.slayerTask == npc.getDefinition().getId()) {
-					player.getSkills().addExperience(Skills.SLAYER, npc.maxHealth * 125);
-					if(-- player.slayerAm <= 0) {
-						player.slayerTask = 0;
-						player.getSkills().addExperience(Skills.SLAYER, 10 * npc.maxHealth * 25);
-						DialogueManager.openDialogue(player, 33);
-					}
-				}
-				//bones
-				if(npc.bones > 0) {
-					GlobalItem globalItem5 = new GlobalItem(
-							player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
-							new Item(npc.bones, 1));
-					World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
-				}
-				//charms
-				if(npc.charm > 0) {
-					GlobalItem globalItem5 = new GlobalItem(
-							player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
-							new Item(npc.charm, 1));
-                    if (player.getInventory().contains(16639))
-                        ContentEntity.addItem(player, npc.charm);
-                    else
-					World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
-				}
-				//talismines
-				int tali = NPCManager.getTalismine(npc.getDefinition());
-				if(tali > 0) {
-					GlobalItem globalItem5 = new GlobalItem(
-							player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
-							new Item(tali, 1)
-					);
-					World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
-				}
-				
-				if(HaloweenEvent.zombies.contains(npc.getDefinition().getId())) {
-					player.sshCharges++;
-					player.sendf("You have @red@%d@bla@ zombie kills", player.sshCharges);
-					if(player.sshCharges%50 == 0) {
-						GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(), 
-								npc.getLocation().getY(), npc.getLocation().getZ(),
-								Item.create(17279));
-						World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
-					}
-					
-					if(Misc.random(1000) < 1) {
-						GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(), 
-								npc.getLocation().getY(), npc.getLocation().getZ(),
-								Item.create(1053 + Misc.random(5)));
-						World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
-					}
+                        /**
+                         * /500 is really low (max of like 6 dp /kill if soloed)
+                         * /60 is also really low (max of like 50 pkp in total for everyone / kill)
+                         */
+                        p.getPoints().inceasePkPoints((int) (p.getCorpDamage() / 12));//1750 hp, 175pkp
+                        p.getPoints().increaseDonatorPoints((int) (p.getCorpDamage() / 150), false);//12 donators pts to divvy up?
+                        if (jet != null) {
+                            jet.getActionSender().sendMessage("" + p.getName() + " has hit: " + p.getCorpDamage());
+                        }
+                    }
+                    p.setCorpDamage(0);
+                }
+            }
+            if (killer != null) {
+                if (!npc.serverKilled) {
+                    World.getWorld().getContentManager().handlePacket(16, killer, npc.getDefinition().getId(), npc.getLocation().getX(), npc.getLocation().getY(), -1);
+                }
+            }
+            npc.setTeleportTarget(npc.getSpawnLocation(), false);
+            if (npc.npcDeathTimer != -1) {
+                timer = 10 + npc.npcDeathTimer;
+                npc.isHidden(true);
+            } else {
+                npc.isHidden(true);
+            }
+            try {
+                if (Summoning.isBoB(npc.getDefinition().getId()) &&
+                        ((Player) World.getWorld().getPlayers().get(npc.ownerId)).cE.summonedNpc == npc)
+                    BoB.dropBoB(npc.getLocation(), (Player) World.getWorld().getPlayers().get(npc.ownerId));
+            } catch (Exception e) {
+            }//it throws a aload of index out of bounds exceptions if a player logs out, handle it if u want i was just a lil lazy at the tiem i c, btw remem , getlevelforexp , if we do binary search with that , its gonna be boostspeed prohax!!!!!! :P guess what, Idk concept of binary search ive heard of it but never  looked at code or implementation ok ill explain in 30 secs
+            if (killer != null) {
+                Player player = killer;
+                if (player.slayerTask == npc.getDefinition().getId()) {
+                    player.getSkills().addExperience(Skills.SLAYER, npc.maxHealth * 125);
+                    if (--player.slayerAm <= 0) {
+                        player.slayerTask = 0;
+                        player.getSkills().addExperience(Skills.SLAYER, 10 * npc.maxHealth * 25);
+                        DialogueManager.openDialogue(player, 33);
+                    }
+                }
+                //bones
+                if (npc.bones > 0) {
+                    GlobalItem globalItem5 = new GlobalItem(
+                            player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
+                            new Item(npc.bones, 1));
+                    World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
+                }
+                //charms
+                if (npc.charm > 0) {
+                    GlobalItem globalItem5 = new GlobalItem(
+                            player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
+                            new Item(npc.charm, 1));
+                    if (player.getInventory().contains(16639)) {
+                        if (!ContentEntity.addItem(player, npc.charm))
+                            World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
+                    } else
+                        World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
+                }
+                //talismines
+                int tali = NPCManager.getTalismine(npc.getDefinition());
+                if (tali > 0) {
+                    GlobalItem globalItem5 = new GlobalItem(
+                            player, npc.getLocation().getX(), npc.getLocation().getY(), npc.getLocation().getZ(),
+                            new Item(tali, 1)
+                    );
+                    World.getWorld().getGlobalItemManager().newDropItem(player, globalItem5);
+                }
 
-					
-				}
-				player.sendf("You now have @red@%d@bla@ %s kills", player.getNPCLogs().log(npc), npc.getDefinition().getName());
+                if (HaloweenEvent.zombies.contains(npc.getDefinition().getId())) {
+                    player.sshCharges++;
+                    player.sendf("You have @red@%d@bla@ zombie kills", player.sshCharges);
+                    if (player.sshCharges % 50 == 0) {
+                        GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(),
+                                npc.getLocation().getY(), npc.getLocation().getZ(),
+                                Item.create(17279));
+                        World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
+                    }
+
+                    if (Misc.random(1000) < 1) {
+                        GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(),
+                                npc.getLocation().getY(), npc.getLocation().getZ(),
+                                Item.create(1053 + Misc.random(5)));
+                        World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
+                    }
+
+
+                }
+                player.sendf("You now have @red@%d@bla@ %s kills", player.getNPCLogs().log(npc), npc.getDefinition().getName());
                 final boolean isTask = player.getSlayer().isTask(npc.getDefinition().getId());
                 //normal drops
-				if(npc.getDefinition().getDrops() != null && npc.getDefinition().getDrops().size() >= 1) {
-                    final int chance =  isTask ? 500 : 1000;
-					for(NPCDrop drop : npc.getDefinition().getDrops()) {
-						if(drop == null) continue;
-						if(Combat.random(chance) <= drop.getChance()) {
-							int amt = drop.getMin() + Combat.random(drop.getMax() - drop.getMin());
-							if(amt < 1) amt = 1; //fix glitched drops
-							GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(), 
-									npc.getLocation().getY(), npc.getLocation().getZ(),
-									Item.create(drop.getId(), amt));
-							if(DonatorShop.getPrice(drop.getId()) > 50) {
-								for(Player p : player.getRegion().getPlayers())
-									p.sendf("@gre@%s has just gotten a %d of %s", player.getName(), amt, ItemDefinition.forId(drop.getId()).getName());
-							}
-							World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
-						}
-					}
+                if (npc.getDefinition().getDrops() != null && npc.getDefinition().getDrops().size() >= 1) {
+                    final int chance = isTask ? 500 : 1000;
+                    for (NPCDrop drop : npc.getDefinition().getDrops()) {
+                        if (drop == null) continue;
+                        if (Combat.random(chance) <= drop.getChance()) {
+                            int amt = drop.getMin() + Combat.random(drop.getMax() - drop.getMin());
+                            if (amt < 1) amt = 1; //fix glitched drops
+                            GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(),
+                                    npc.getLocation().getY(), npc.getLocation().getZ(),
+                                    Item.create(drop.getId(), amt));
+                            if (DonatorShop.getPrice(drop.getId()) > 50) {
+                                for (Player p : player.getRegion().getPlayers())
+                                    p.sendf("@gre@%s has just gotten a %d of %s", player.getName(), amt, ItemDefinition.forId(drop.getId()).getName());
+                            }
+                            World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
+                        }
+                    }
 
-				}
-                if(isTask && Misc.random(1000) < 1) {
+                }
+                if (isTask && Misc.random(1000) < 1) {
                     GlobalItem globalItem = new GlobalItem(player, npc.getLocation().getX(),
                             npc.getLocation().getY(), npc.getLocation().getZ(),
                             Item.create(18768, 1));
                     World.getWorld().getGlobalItemManager().newDropItem(player, globalItem);
 
                 }
-			}
-		} else if(timer == - 1) {
-			World.getWorld().unregister(npc);
-			this.stop();
-		} else if(timer == 10) {
-			npc.isHidden(false);
-			npc.playAnimation(Animation.create(- 1, 0));
-			npc.setDead(false);
-			npc.cE.setFreezeTimer(0);
-			npc.health = npc.maxHealth;
-			this.stop();
-		}
-		timer--;
-	}
+            }
+        } else if (timer == -1) {
+            World.getWorld().unregister(npc);
+            this.stop();
+        } else if (timer == 10) {
+            npc.isHidden(false);
+            npc.playAnimation(Animation.create(-1, 0));
+            npc.setDead(false);
+            npc.cE.setFreezeTimer(0);
+            npc.health = npc.maxHealth;
+            this.stop();
+        }
+        timer--;
+    }
 
 
 }

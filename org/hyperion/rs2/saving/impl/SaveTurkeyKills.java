f1a376bf839c8668b60eac8c4dae20821dbf3d4f
fuzenseth <paras-best@hotmail.com>
2014-11-17 14:33:37 +0200

    Saveturkeykills

---
+++ org/hyperion/rs2/saving/impl/SaveTurkeyKills.java
@@ -0,0 +1,29 @@
+package org.hyperion.rs2.saving.impl;
+
+import org.hyperion.rs2.model.Player;
+import org.hyperion.rs2.saving.SaveInteger;
+
+public class SaveTurkeyKills extends SaveInteger {
+
+    public SaveTurkeyKills(String name) {
+        super(name);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int getDefaultValue() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setValue(Player player, int value) {
+        player.turkeyKills = value;
+    }
+
+    @Override
+    public Integer getValue(Player player) {
+        return player.turkeyKills;
+    }
+
+}

fca8a95778955d6e32fa6afe32b196157bf851af
deviouspk <aamining2@gmail.com>
2014-11-22 03:17:27 +0100

    server push

--- org/hyperion/rs2/model/combat/Combat.java
+++ org/hyperion/rs2/model/combat/Combat.java
@@ -769,11 +769,13 @@ public class Combat {
 		World.getWorld().submit(new Event(delay, "npcatx") {
 			@Override
 			public void execute() {
-                
-                if (combatEntity.getEntity() != null && combatEntity.getOpponent() != null) {
-                    if (combatEntity.getEntity().isDead() || combatEntity.getOpponent().getEntity().isDead())
-                        this.stop();
-                        return;
+
+                if (combatEntity == null ||
+                        combatEntity.getEntity() == null ||
+                        combatEntity.getOpponent() == null ||
+                        npc == null) {
+                    this.stop();
+                    return;
                 }
 				int newDamg = SpiritShields.applyEffects(combatEntity, damg);;
 				if(combatEntity.getEntity() instanceof Player) {

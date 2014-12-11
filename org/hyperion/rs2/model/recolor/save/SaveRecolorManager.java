8b0e281771aab71bbe0a40cecaf0a80373a1d04d
dvpkjoshy <joshua.maione@hotmail.com>
2014-12-11 11:11:39 -0500

    lol

--- org/hyperion/rs2/model/recolor/save/SaveRecolorManager.java
+++ org/hyperion/rs2/model/recolor/save/SaveRecolorManager.java
@@ -3,6 +3,7 @@ package org.hyperion.rs2.model.recolor.save;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.List;
 import org.hyperion.rs2.model.Player;
 import org.hyperion.rs2.model.recolor.Recolor;
 import org.hyperion.rs2.saving.SaveObject;
@@ -16,9 +17,10 @@ public class SaveRecolorManager extends SaveObject{
     public boolean save(final Player player, final BufferedWriter writer) throws IOException{
         writer.write(getName());
         writer.newLine();
-        writer.write(Integer.toString(player.getRecolorManager().getCount()));
+        final List<Recolor> recolors = player.getRecolorManager().getAll();
+        writer.write(Integer.toString(recolors.size()));
         writer.newLine();
-        for(final Recolor recolor : player.getRecolorManager().getAll()){
+        for(final Recolor recolor : recolors){
             writer.write(recolor.toString());
             writer.newLine();
         }

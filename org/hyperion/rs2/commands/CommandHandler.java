9f40aafd33f2aa69d5cdbc4fc1246b5c2e7c229d
dvpkjoshy <joshua.maione@hotmail.com>
2014-12-16 14:34:33 -0500

    wildy forbid punishment type

--- org/hyperion/rs2/commands/CommandHandler.java
+++ org/hyperion/rs2/commands/CommandHandler.java
@@ -954,6 +954,10 @@ public class CommandHandler {
         CommandHandler.submit(new PunishCommand("ipban", Target.IP, Type.BAN, Rank.GLOBAL_MODERATOR));
         CommandHandler.submit(new PunishCommand("macban", Target.MAC, Type.BAN, Rank.ADMINISTRATOR));
 
+        CommandHandler.submit(new PunishCommand("wildyforbid", Target.ACCOUNT, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
+        CommandHandler.submit(new PunishCommand("ipwildyforbid", Target.IP, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
+        CommandHandler.submit(new PunishCommand("macwildyforbid", Target.MAC, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
+
         CommandHandler.submit(new UnPunishCommand("unjail", Target.ACCOUNT, Type.JAIL, Rank.HELPER));
         CommandHandler.submit(new UnPunishCommand("unipjail", Target.IP, Type.JAIL, Rank.HELPER));
         CommandHandler.submit(new UnPunishCommand("unmacjail", Target.MAC, Type.JAIL, Rank.HELPER));
@@ -969,6 +973,10 @@ public class CommandHandler {
         CommandHandler.submit(new UnPunishCommand("unban", Target.ACCOUNT, Type.BAN, Rank.MODERATOR));
         CommandHandler.submit(new UnPunishCommand("unipban", Target.IP, Type.BAN, Rank.GLOBAL_MODERATOR));
         CommandHandler.submit(new UnPunishCommand("unmacban", Target.MAC, Type.BAN, Rank.ADMINISTRATOR));
+
+        CommandHandler.submit(new UnPunishCommand("unwildyforbid", Target.ACCOUNT, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
+        CommandHandler.submit(new UnPunishCommand("unipwildyforbid", Target.IP, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
+        CommandHandler.submit(new UnPunishCommand("unmacwildyforbid", Target.MAC, Type.WILDY_FORBID, Rank.ADMINISTRATOR));
 
         CommandHandler.submit(new CheckPunishmentCommand());
         CommandHandler.submit(new ViewPunishmentsCommand());

32b0ca29f82125b9f1a5c6db764e24eef64a4e5b
Alsk Jet <swm321@gmail.com>
2014-12-20 04:34:44 -0500

    fix app shitz

--- org/hyperion/rs2/packet/PlayerOptionPacketHandler.java
+++ org/hyperion/rs2/packet/PlayerOptionPacketHandler.java
@@ -118,7 +118,7 @@ public class PlayerOptionPacketHandler implements PacketHandler {
 					ContentEntity.startAnimation(player, 7530);
 					SnowItems.fireSnowBall(player, victim);
 					player.getWalkingQueue().reset();
-					World.getWorld().submit(new Event(1000) {
+					World.getWorld().submit(new Event(2000) {
 						public void execute() {
 							if(player.getInventory().getCount(SnowItems.SNOWBALL.getId()) > 0)
 								player.getInventory().remove(new Item(11951, 1));

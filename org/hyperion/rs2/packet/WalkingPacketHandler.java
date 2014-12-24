b9fe0bd4fd40ec06b5b06b496a4ec205e83c4733
Alsk Jet <swm321@gmail.com>
2014-12-24 16:40:49 -0500

    debugging walk

--- org/hyperion/rs2/packet/WalkingPacketHandler.java
+++ org/hyperion/rs2/packet/WalkingPacketHandler.java
@@ -113,7 +113,8 @@ public class WalkingPacketHandler implements PacketHandler {
 		for(int i = 0; i < steps; i++) {
 			path[i][0] += firstX;
 			path[i][1] += firstY;
-			player.getWalkingQueue().addStep(path[i][0], path[i][1]);
+            System.out.printf("Steps: %d FirstX: %d FirstY: %d Path WalkX: %d Path WalkY: %d", i, firstX, firstY, path[i][0], path[i][1]);
+            player.getWalkingQueue().addStep(path[i][0], path[i][1]);
 		}
 		player.getWalkingQueue().finish();
 

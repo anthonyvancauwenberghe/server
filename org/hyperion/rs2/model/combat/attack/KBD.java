68da90cc61b74204070faf631a570593605d3e54
Alsk Jet <swm321@gmail.com>
2014-12-24 17:26:38 -0500

    kbd multi

--- org/hyperion/rs2/model/combat/attack/KBD.java
+++ org/hyperion/rs2/model/combat/attack/KBD.java
@@ -25,6 +25,7 @@ public class KBD implements Attack {
             n.getDefinition().getBonus()[4] = 400;
             n.getDefinition().getBonus()[3] = 400;
 			int attackId = Combat.random(10);
+            for(final Player p : attack.getEntity().getRegion().getPlayers()) {
 	        /*
 			 * 
 			 * 393 = KBD = red projectile
@@ -32,48 +33,49 @@ public class KBD implements Attack {
 				395 = KBD = white projectile
 				396 = KBD = blue projectile
 			 */
-			if(attackId > 4 && distance <= (1 + (n.getDefinition().sizeX() + n.getDefinition().sizeY()) / 2)) {
+			    if(attackId > 4 && distance <= (1 + (n.getDefinition().sizeX() + n.getDefinition().sizeY()) / 2)) {
 				//melee
-				n.cE.doAnim(n.getDefinition().getAtkEmote(1));
-				n.cE.predictedAtk = (System.currentTimeMillis() + 2000);
-				Combat.npcAttack(n, attack, CombatCalculation.getCalculatedDamage(n, attack.getEntity(), Combat.random(40), Constants.MELEE, 40), 500, Constants.MELEE);
-			} else if(attackId <= 1) {
-				//posison
-				n.cE.doAnim(n.getDefinition().getAtkEmote(2));
-				n.cE.predictedAtk = (System.currentTimeMillis() + 2000);
-				Combat.npcAttack(n, attack, CombatCalculation.getCalculatedDamage(n, attack.getEntity(), Combat.random(37), Constants.RANGE, 37), 1000, Constants.RANGE);
-				if(n.getDefinition().getId() == 50)
-					Combat.poisonEntity(attack);
-				Combat.npcRangeAttack(n, attack, 394, 40, false);
-			} else if(attackId > 1 && attackId <= 4) {
+				    n.cE.doAnim(n.getDefinition().getAtkEmote(1));
+				    n.cE.predictedAtk = (System.currentTimeMillis() + 2000);
+				    Combat.npcAttack(n, p.cE, CombatCalculation.getCalculatedDamage(n, p, Combat.random(47), Constants.MELEE, 47), 500, Constants.MELEE);
+			    } else if(attackId <= 1) {
+				    //posison
+				    n.cE.doAnim(n.getDefinition().getAtkEmote(2));
+				    n.cE.predictedAtk = (System.currentTimeMillis() + 2000);
+				    Combat.npcAttack(n, p.cE, CombatCalculation.getCalculatedDamage(n, p, Combat.random(37), Constants.RANGE, 37), 1000, Constants.RANGE);
+				    if(n.getDefinition().getId() == 50)
+					    Combat.poisonEntity(p.cE);
+				    Combat.npcRangeAttack(n, p.cE, 394, 40, false);
+			    } else if(attackId > 1 && attackId <= 4) {
 				//ice freeze
-				n.cE.doAnim(n.getDefinition().getAtkEmote(2));
-				n.cE.predictedAtk = (System.currentTimeMillis() + 1800);
-				Combat.npcAttack(n, attack, CombatCalculation.getCalculatedDamage(n, attack.getEntity(), Combat.random(45), Constants.MAGE, 45), 1000, Constants.MAGE);
-				if(n.getDefinition().getId() == 50 && Combat.random(2) == 1) {
-                    if(attack.canBeFrozen())
-					    attack.setFreezeTimer(20000);
-                }
-				Combat.npcRangeAttack(n, attack, 396, 40, false);
-			} else {
+				    n.cE.doAnim(n.getDefinition().getAtkEmote(2));
+				    n.cE.predictedAtk = (System.currentTimeMillis() + 1800);
+				    Combat.npcAttack(n, p.cE, CombatCalculation.getCalculatedDamage(n, p, Combat.random(45), Constants.MAGE, 45), 1000, Constants.MAGE);
+				    if(n.getDefinition().getId() == 50 && Combat.random(2) == 1) {
+                        if(p.cE.canBeFrozen())
+					        p.cE.setFreezeTimer(20000);
+                    }
+				    Combat.npcRangeAttack(n, p.cE, 396, 40, false);
+			    } else {
 				//firebreath
-				n.cE.doAnim(n.getDefinition().getAtkEmote(2));
-				n.cE.predictedAtk = (System.currentTimeMillis() + 1800);
+				    n.cE.doAnim(n.getDefinition().getAtkEmote(2));
+				    n.cE.predictedAtk = (System.currentTimeMillis() + 1800);
 
-				boolean antiFire = (System.currentTimeMillis() - attack.getPlayer().antiFireTimer < 360000) && attack.getPlayer().superAntiFire;
-                if (System.currentTimeMillis() - attack.getPlayer().antiFireTimer < 360000 && attack.getPlayer().superAntiFire) {
-                        Combat.npcAttack(n, attack.getOpponent(), 0, 1000, 3);
-                } else if(attack.getPlayer().getEquipment().get(Equipment.SLOT_SHIELD) != null && (attack.getPlayer().getEquipment().get(Equipment.SLOT_SHIELD).getId() == 1540 || attack.getPlayer().getEquipment().get(Equipment.SLOT_SHIELD).getId() == 11283 || attack.getPlayer().getEquipment().get(Equipment.SLOT_SHIELD).getId() == 11284)) {
-					if(System.currentTimeMillis() - attack.getPlayer().antiFireTimer < 360000) {
-						Combat.npcAttack(n, attack, 0, 1000, 3);
-                    } else
-						Combat.npcAttack(n, attack, Combat.random(10), 1000, 3);
-                } else if(System.currentTimeMillis() - attack.getPlayer().antiFireTimer < 360000)
-					Combat.npcAttack(n, attack, Combat.random(10), 1000, 3);
-				else
-					Combat.npcAttack(n, attack, Combat.random(60), 1000, 3);
-				Combat.npcRangeAttack(n, attack, 393, 40, false);
-			}
+				    boolean antiFire = (System.currentTimeMillis() - p.antiFireTimer < 360000) && p.superAntiFire;
+                    if (System.currentTimeMillis() - p.antiFireTimer < 360000 && p.superAntiFire) {
+                            Combat.npcAttack(n, p.cE.getOpponent(), 0, 1000, 3);
+                    } else if(p.getEquipment().get(Equipment.SLOT_SHIELD) != null && (p.getEquipment().get(Equipment.SLOT_SHIELD).getId() == 1540 || p.getEquipment().get(Equipment.SLOT_SHIELD).getId() == 11283 || p.getEquipment().get(Equipment.SLOT_SHIELD).getId() == 11284)) {
+					    if(System.currentTimeMillis() - p.antiFireTimer < 360000) {
+						    Combat.npcAttack(n, p.cE, 0, 1000, 3);
+                        } else
+						    Combat.npcAttack(n, p.cE, Combat.random(10), 1000, 3);
+                    } else if(System.currentTimeMillis() - p.antiFireTimer < 360000)
+					    Combat.npcAttack(n, p.cE, Combat.random(10), 1000, 3);
+				    else
+					    Combat.npcAttack(n, p.cE, Combat.random(60), 1000, 3);
+				    Combat.npcRangeAttack(n, p.cE, 393, 40, false);
+			    }
+            }
 
 			return 5;
 		} else if(n.getLocation().isWithinDistance(n.cE.getOpponent().getEntity().getLocation(), 15)) {

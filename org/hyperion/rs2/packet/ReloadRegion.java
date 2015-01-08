18263634019b421afc6084f7fad4138a128f25e6
Alsk Jet <swm321@gmail.com>
2015-01-08 11:06:32 -0500

    changing regions packet

--- org/hyperion/rs2/packet/ReloadRegion.java
+++ org/hyperion/rs2/packet/ReloadRegion.java
@@ -1,6 +1,7 @@
 package org.hyperion.rs2.packet;
 
 import org.hyperion.rs2.model.Player;
+import org.hyperion.rs2.model.Rank;
 import org.hyperion.rs2.model.World;
 import org.hyperion.rs2.model.content.DoorManager;
 import org.hyperion.rs2.model.content.skill.Farming;
@@ -14,10 +15,13 @@ public class ReloadRegion
 
 	@Override
 	public void handle(Player player, Packet packet) {
+        final long timeStart = System.currentTimeMillis();
 		DoorManager.refresh(player);
 		World.getWorld().getGlobalItemManager().displayItems(player);
 		Farming.farming.refreshFarmObjects(player);
 		World.getWorld().getObjectMap().load(player);
 		player.getWalkingQueue().reset();
+        if(Rank.hasAbility(player, Rank.HELPER))
+            System.out.printf("Reload region time for %s is %d\n", player.getName(), System.currentTimeMillis() - timeStart);
 	}
 }

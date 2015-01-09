77edd25ef0ab80d065ae009493e146126fb7a8af
deviouspk <aamining2@gmail.com>
2015-01-09 14:57:54 +0100

    changes

--- org/hyperion/rs2/sql/LogsSQLConnection.java
+++ org/hyperion/rs2/sql/LogsSQLConnection.java
@@ -1,29 +1,28 @@
 package org.hyperion.rs2.sql;
 
 import org.hyperion.Configuration;
-import org.hyperion.Server;
 import org.hyperion.rs2.sql.event.impl.LogPlayercountEvent;
 
 public class LogsSQLConnection extends MySQLConnection {
 
-	public LogsSQLConnection(Configuration config) {
-		super("LogsSQL", config.getString("logsurl"), config.getString("logsuser"), config.getString("logspass"), 30000,
-				10000, 100);
-	}
+    public LogsSQLConnection(Configuration config) {
+        super("LogsSQL", config.getString("logsurl"), config.getString("logsuser"), config.getString("logspass"), 30000,
+                10000, 100);
+    }
 
 
-	public LogsSQLConnection(String url, String username, String password) {
-		super("LogsSQL", url, username, password, 30000,
-				10000, 100);
-	}
+    public LogsSQLConnection(String url, String username, String password) {
+        super("LogsSQL", url, username, password, 30000,
+                10000, 100);
+    }
 
-	@Override
-	public boolean init() {
-	    /*if(!Server.getConfig().getBoolean("sql"))
+    @Override
+    public boolean init() {
+        /*if(!Server.getConfig().getBoolean("sql"))
 			return false;*/
-		establishConnection();
-		submit(new LogPlayercountEvent());
-		start();
-		return true;
-	}
+        establishConnection();
+        submit(new LogPlayercountEvent());
+        start();
+        return true;
+    }
 }

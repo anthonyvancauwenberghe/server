0cc83edad1487a5fbae5182d150840eb90101b0e
deviouspk <aamining2@gmail.com>
2015-01-12 06:03:17 +0100

    fixing my terrible coding

--- org/hyperion/rs2/model/container/duel/Duel.java
+++ org/hyperion/rs2/model/container/duel/Duel.java
@@ -192,7 +192,7 @@ public class Duel {
             return;
         }
 
-        if(player.getTrader() != null && player.isNewlyCreated() && player.hardMode() || player.getTrader().isNewlyCreated() && player.getTrader().hardMode()) {
+        if(player != null && player.getTrader() != null && player.isNewlyCreated() && player.hardMode() || player.getTrader().isNewlyCreated() && player.getTrader().hardMode()) {
             player.sendMessage("You or your partner is too new to stake");
             return;
         }

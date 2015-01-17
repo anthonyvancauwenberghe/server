c1c7acbebf24d73ef3d637e20f1ffc9d97d75b75
Alsk Jet <swm321@gmail.com>
2015-01-16 21:04:02 -0500

    80k to 100k

--- org/hyperion/rs2/model/shops/LegendaryStore.java
+++ org/hyperion/rs2/model/shops/LegendaryStore.java
@@ -47,7 +47,7 @@ public class LegendaryStore extends CurrencyShop {
                     for(final Item item : container.toArray()) {
                         if(item == null) continue;
                         int price = getSpecialPrice(item);
-                        writer.write(String.format("[%s]: PRICE: %d PKP: %,d DP: %,d", item.getDefinition().getName(), price, price * 80_000, price * 1000));
+                        writer.write(String.format("[%s]: PRICE: %d PKP: %,d DP: %,d", item.getDefinition().getName(), price, price * 100_000, price * 1000));
                         writer.newLine();
                     }
                 } catch(final Exception ex) {

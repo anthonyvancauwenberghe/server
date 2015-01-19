d38b54f885bfd516ee87163c7d91de4f3c6eb7f4
deviouspk <aamining2@gmail.com>
2015-01-19 17:42:58 +0100

    npe fix & sql shizzle

--- org/hyperion/rs2/sql/event/impl/LogServerWealthEvent.java
+++ org/hyperion/rs2/sql/event/impl/LogServerWealthEvent.java
@@ -13,7 +13,7 @@ public class LogServerWealthEvent extends SQLEvent {
     }
 
     public void execute(SQLConnection con) throws SQLException {
-        con.query("INSERT INTO serverwealth (value) VALUES ((SELECT SUM(value) FROM accountvalues))");
+        con.query("INSERT INTO serverwealth (value) VALUES ((SELECT SUM(value) FROM accountvalues WHERE value>15))");
         super.updateStartTime();
     }
 }

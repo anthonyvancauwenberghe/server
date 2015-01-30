744b1b6275477795ba63bbecc2decff074f42aca
Alsk Jet <swm321@gmail.com>
2015-01-29 21:34:22 -0500

    local players

--- org/hyperion/rs2/model/combat/attack/AvatarOfDestruction.java
+++ org/hyperion/rs2/model/combat/attack/AvatarOfDestruction.java
@@ -48,6 +48,7 @@ public class AvatarOfDestruction implements Attack {
         }
         n.getCombat().doAtkEmote();
         for(final Player player : n.getLocalPlayers()) {
+            System.out.println(player.getName());
             int hitType = Combat.random(1);
             int tempDamage = CombatCalculation.getCalculatedDamage(n, player, hitType, Constants.MELEE, MAX_DAMAGE);
             if(player.getLocation().distance(n.getLocation()) == 1)

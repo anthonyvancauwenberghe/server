d4a30a4ed06187a8494fbe6c165b2481f32d2909
Alsk Jet <swm321@gmail.com>
2015-02-03 22:42:40 -0500

    unnerf melee

--- org/hyperion/rs2/model/combat/CombatAssistant.java
+++ org/hyperion/rs2/model/combat/CombatAssistant.java
@@ -1255,7 +1255,7 @@ public class CombatAssistant {
 				bonus = player.getBonus().get(i);
 			}
 		}
-		return (int) ((meleeAtk + (bonus/1.5) * .75));
+		return (int) ((meleeAtk + (bonus/1.5) * .9));
 	}
 
 	/**

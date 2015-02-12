cb4babe3d160eb1711ba2e78fdfa3caf45e68c3d
Alsk Jet <swm321@gmail.com>
2015-02-11 20:30:43 -0500

    print out map

--- org/hyperion/rs2/model/combat/CombatEntity.java
+++ org/hyperion/rs2/model/combat/CombatEntity.java
@@ -219,6 +219,8 @@ public class CombatEntity {
 		int max = 0;
 		for(Map.Entry<String, Integer> entry : damageDealt.entrySet()) {
 			int damage = entry.getValue();
+            if(entity instanceof NPC)
+                System.out.println("Winner: "+entry.getKey() + " Value: "+entry.getValue() + " For npc "+ getNPC().getDefinition().getId());
 			if(damage > max) {
 				winner = entry.getKey();
 				max = damage;

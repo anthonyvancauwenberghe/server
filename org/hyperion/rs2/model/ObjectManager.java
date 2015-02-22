b563b6056edd2855cbb70429dd3feaa98ae854a1
Alsk Jet <swm321@gmail.com>
2015-02-22 09:15:11 -0500

    duh jank

--- org/hyperion/rs2/model/ObjectManager.java
+++ org/hyperion/rs2/model/ObjectManager.java
@@ -62,7 +62,7 @@ public class ObjectManager implements LandscapeListener, ObjectDefinitionListene
 			while((s = br.readLine()) != null){
 				try {
 					String parts[] = s.replace("spawn = ","").split("\t");
-					globalObjects.add(new GameObject(GameObjectDefinition.forId(Integer.parseInt(parts[0])), Location.create(Integer.parseInt(parts[1]),Integer.parseInt(parts[2]),Integer.parseInt(parts[3])), Integer.parseInt(parts[4]), Integer.parseInt(parts[5])));
+					globalObjects.add(new GameObject(GameObjectDefinition.forId(Integer.parseInt(parts[0])), Location.create(Integer.parseInt(parts[1]),Integer.parseInt(parts[2]),Integer.parseInt(parts[3])), Integer.parseInt(parts[5]), Integer.parseInt(parts[4])));
 				} catch(Exception e){
 					e.printStackTrace();
 				}

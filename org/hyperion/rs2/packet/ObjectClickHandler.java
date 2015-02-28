b2c5f68026b4d3e4bf69f10d5e214771a8b158be
Alsk Jet <swm321@gmail.com>
2015-02-27 20:40:44 -0500

    push dat shit

--- org/hyperion/rs2/packet/ObjectClickHandler.java
+++ org/hyperion/rs2/packet/ObjectClickHandler.java
@@ -21,11 +21,6 @@ public class ObjectClickHandler {
 			p.getActionSender().sendMessage("You are too far away from the object to interact with it!");
 			return;
 		}
-
-        if(!World.getWorld().isWalkAble(p.getLocation().getZ()%4, p.getLocation().getX(), p.getLocation().getY(), x, y, 1)) {
-            p.getActionSender().sendMessage("You are too far away from the object to interact with it!");
-            return;
-        }
 		if(Rank.hasAbility(p, Rank.ADMINISTRATOR) && p.debug)
 			p.getActionSender().sendMessage("Clicked object: "+id);
 		if(World.getWorld().getContentManager().handlePacket(5 + type, p, id, x, y, - 1))

fefb074108b59306e4155d7fb6d21168ba0c3f2a
Alsk Jet <swm321@gmail.com>
2015-03-03 18:58:14 -0500

    chaos ele fix

--- org/hyperion/rs2/model/combat/attack/CommonMagicAttack.java
+++ org/hyperion/rs2/model/combat/attack/CommonMagicAttack.java
@@ -5,6 +5,7 @@ import org.hyperion.rs2.model.Location;
 import org.hyperion.rs2.model.NPC;
 import org.hyperion.rs2.model.Player;
 import org.hyperion.rs2.model.combat.Combat;
+import org.hyperion.rs2.model.combat.CombatCalculation;
 import org.hyperion.rs2.model.combat.CombatEntity;
 import org.hyperion.util.Misc;
 
@@ -17,7 +18,8 @@ public class CommonMagicAttack implements Attack {
 	public int handleAttack(final NPC n, final CombatEntity attack) {
 		int distance = attack.getEntity().getLocation().distance((Location.create(n.cE.getEntity().getLocation().getX() + n.cE.getOffsetX(), n.cE.getEntity().getLocation().getY() + n.cE.getOffsetY(), n.cE.getEntity().getLocation().getZ())));
 		if(distance < (8 + ((n.getDefinition().sizeX() + n.getDefinition().sizeY()) / 2))) {
-			Combat.npcAttack(n, attack, Misc.random(35), 1000, Misc.random(2));
+            final int type = Misc.random(2);
+			Combat.npcAttack(n, attack, CombatCalculation.getCalculatedDamage(n, attack.getEntity(), Misc.random(28), type, 28), 1000, type);
 			if(Misc.random(3) == 0 && attack.getEntity() instanceof Player) {
 				handleEffect(attack.getPlayer());
 			}

ae212b0d216914f04bb6cee14312ccbd4bf93601
Alsk Jet <swm321@gmail.com>
2015-03-08 17:46:25 -0400

    reduce player boost by 2%

--- org/hyperion/rs2/model/World.java
+++ org/hyperion/rs2/model/World.java
@@ -96,7 +96,7 @@ import org.hyperion.util.BlockingExecutorService;
  */
 public class World {
 
-    public static final double PLAYER_MULTI = 1.13;
+    public static final double PLAYER_MULTI = 1.11;
 
     /**
      * Ticket Manager - no fuckin shit

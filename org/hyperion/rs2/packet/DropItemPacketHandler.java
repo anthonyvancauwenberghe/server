e0ac76027c639eada4273000f9c4269cd841a29e
Alsk Jet <swm321@gmail.com>
2015-03-15 16:12:25 -0400

    check if can spawn for drop

--- org/hyperion/rs2/packet/DropItemPacketHandler.java
+++ org/hyperion/rs2/packet/DropItemPacketHandler.java
@@ -5,6 +5,7 @@ import org.hyperion.rs2.model.ItemDefinition;
 import org.hyperion.rs2.model.Player;
 import org.hyperion.rs2.model.Rank;
 import org.hyperion.rs2.model.World;
+import org.hyperion.rs2.model.content.misc.ItemSpawning;
 import org.hyperion.rs2.net.Packet;
 
 public class DropItemPacketHandler implements PacketHandler {
@@ -18,7 +19,7 @@ public class DropItemPacketHandler implements PacketHandler {
 		int itemSlot = packet.getShortA();
 		if(itemId < 0 || itemId > ItemDefinition.MAX_ID || itemSlot < 0 || itemSlot > 27)
 			return;
-		if(player.isDead() || System.currentTimeMillis() - player.cE.lastHit < 10000) {
+		if((player.isDead() || System.currentTimeMillis() - player.cE.lastHit < 10000) && !ItemSpawning.canSpawn(itemId)) {
 			player.getActionSender().sendMessage("You can't drop items, while in combat.");
 			return;
 		}

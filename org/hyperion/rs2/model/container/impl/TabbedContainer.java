cd9e302f25ffb06305f637c989d92313306309da
Alsk Jet <swm321@gmail.com>
2015-03-21 16:31:03 -0400

    gg bunk

--- org/hyperion/rs2/model/container/impl/TabbedContainer.java
+++ org/hyperion/rs2/model/container/impl/TabbedContainer.java
@@ -108,6 +108,14 @@ public class TabbedContainer extends Container {
         Item[] old = items;
         items = new Item[capacity];
         int newIndex = 0;
+        Arrays.fill(player.getBankField().getTabAmounts(), 0);
+        for(int i = 0; i < items.length; i++) {
+            if(old[i] != null) {
+                items[newIndex] = old[i];
+                player.getBankField().getTabAmounts()[((BankItem)items[newIndex]).getTabIndex()]++;
+                newIndex++;
+            }
+        }
         if(isFiringEvents()) {
             fireItemsChanged();
         }

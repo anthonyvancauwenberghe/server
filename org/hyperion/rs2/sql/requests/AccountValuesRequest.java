e0dd2a925f7dfad724251eeb2aad3213c005fa4a
Alsk Jet <swm321@gmail.com>
2015-04-12 20:57:47 -0400

    farming saing

--- org/hyperion/rs2/sql/requests/AccountValuesRequest.java
+++ org/hyperion/rs2/sql/requests/AccountValuesRequest.java
@@ -5,6 +5,8 @@ import org.hyperion.rs2.sql.SQLConnection;
 import org.hyperion.rs2.sql.SQLRequest;
 import org.hyperion.util.Time;
 
+import java.util.Arrays;
+
 public class AccountValuesRequest extends SQLRequest{
 
     public AccountValuesRequest(final Player player){
@@ -15,15 +17,22 @@ public class AccountValuesRequest extends SQLRequest{
     public void process(final SQLConnection sql){
         if(System.currentTimeMillis() - player.lastAccountValueTime < Time.ONE_MINUTE)
             return;
-        final int value = player.getAccountValue().getTotalValue();
+        final int value = player.getAccountValue().getTotalValue(), mac = player.getUID();
         final long pkpValue = player.getAccountValue().getPkPointValue();
+        final String ip = player.getShortIP(), specialUid = Arrays.toString(player.specialUid);
         final String query = String.format(
-                "INSERT INTO accountvalues (name, value, pkvalue) VALUES ('%s', %d, %d) ON DUPLICATE KEY UPDATE value = %d, pkvalue = %d",
+                "INSERT INTO accountvalues (name, value, pkvalue,ip,mac,suid) VALUES ('%s', %d, %d, '%s', %d, '%s') ON DUPLICATE KEY UPDATE value = %d, pkvalue = %d ip=%s mac=%d suid=%s",
                 player.getName().toLowerCase(),
                 value,
                 pkpValue,
+                ip,
+                mac,
+                specialUid,
                 value,
-                pkpValue
+                pkpValue,
+                ip,
+                mac,
+                specialUid
         );
         try{
             if (player.getName().toLowerCase() != "thomas" || player.getName().toLowerCase() != "rwt bank"){

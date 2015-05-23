d134a4ba1aeee3c4c7d2b1d8ab9a1cbf7d212f34
Alsk Jet <swm321@gmail.com>
2015-05-23 03:58:10 -0400

    exit lobby

--- org/hyperion/rs2/model/content/minigame/barrowsffa/BarrowSet.java
+++ org/hyperion/rs2/model/content/minigame/barrowsffa/BarrowSet.java
@@ -9,20 +9,20 @@ import static org.hyperion.rs2.model.content.minigame.barrowsffa.BarrowsFFA.DIAL
 import java.util.stream.Stream;
 
 public enum BarrowSet {
-    DHAROK(DIALOGUE_ID + 1, new Integer(4716), new Integer(4718), new Integer(4720), new Integer(4722)),
-    KARIL(DIALOGUE_ID + 2, new Integer(4732), new Integer(4734), new Integer(4736), new Integer(4738)),
-    AHRIM(DIALOGUE_ID + 3, new Integer(4714), new Integer(4712), new Integer(4710), new Integer(4708)),
-    GUTHAN(DIALOGUE_ID + 5, new Integer(4724), new Integer(4726), new Integer(4728), new Integer(4730)),
-    TORAGS(DIALOGUE_ID + 6, new Integer(4745), new Integer(4747), new Integer(4749), new Integer(4751)),
-    VERACS(DIALOGUE_ID + 7, new Integer(4753), new Integer(4755), new Integer(4757), new Integer(4759));
+    DHAROK(DIALOGUE_ID + 1, Item.create(4716), Item.create(4718), Item.create(4720), Item.create(4722)),
+    KARIL(DIALOGUE_ID + 2, Item.create(4732), Item.create(4734), Item.create(4736), Item.create(4738), Item.create(4740, 100)),
+    AHRIM(DIALOGUE_ID + 3, Item.create(4714), Item.create(4712), Item.create(4710), Item.create(4708), Item.create(555, 1000), Item.create(560, 500), Item.create(565, 250)),
+    GUTHAN(DIALOGUE_ID + 5, Item.create(4724), Item.create(4726), Item.create(4728), Item.create(4730)),
+    TORAGS(DIALOGUE_ID + 6, Item.create(4745), Item.create(4747), Item.create(4749), Item.create(4751)),
+    VERACS(DIALOGUE_ID + 7, Item.create(4753), Item.create(4755), Item.create(4757), Item.create(4759));
 
     public static final BarrowSet[] SETS = values().clone();
 
     private final Item[] items;
     private final int dialogueId; //dialogue id for picking the set
 
-    private BarrowSet(int dialogueAction ,final Integer... ids) {
-        this.items = Stream.of(ids).map(Item::create).toArray(Item[]::new);
+    private BarrowSet(int dialogueAction ,final Item... ids) {
+        this.items = ids;
         this.dialogueId = dialogueAction;
     }
 

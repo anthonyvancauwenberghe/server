c3ad0bb72e9c52873bb2bff603337088b4ed64d2
Alsk Jet <swm321@gmail.com>
2015-06-07 19:53:21 -0400

    BORK shit & removing shit

--- org/hyperion/rs2/model/combat/attack/BorkAndMinions.java
+++ org/hyperion/rs2/model/combat/attack/BorkAndMinions.java
@@ -25,7 +25,7 @@ public class BorkAndMinions implements Attack {
         NPCDefinition.getDefinitions()[BORK_ID] =
                 NPCDefinition.create(BORK_ID, 600, 425, bonus, 8756, 8755, new int[]{8754}, 3, "Bork", 120);
         NPCDefinition.getDefinitions()[7135] =
-                NPCDefinition.create(7135, 600, 425, bonus, 8762, 8761, new int[]{8760}, 3, "Borklets", 120);
+                NPCDefinition.create(7135, 125, 425, bonus, 8762, 8761, new int[]{8760}, 3, "Borklets", 120);
     }
     @Override
     public String getName() {

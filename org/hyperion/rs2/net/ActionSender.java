bce2a3987cf82415f3fbdabe37c88f2a61986dbf
Alsk Jet <swm321@gmail.com>
2015-06-18 01:19:58 -0400

    comm channel

--- org/hyperion/rs2/net/ActionSender.java
+++ org/hyperion/rs2/net/ActionSender.java
@@ -215,7 +215,7 @@ public class ActionSender {
             if(player.getTutorialProgress() == 0) {
                 player.setTutorialProgress(7);
             }
-            player.sendMessage("@bla@Welcome Back To @red@Artero! @bla@Happy Playing!", "@red@Subscribe to our Community Channel: @blu@ http://j.mp/apkchannel#url# ", "@red@Runelocus TEMPORARILY gives 3 vote boxes per vote!");
+            player.sendMessage("@bla@Welcome Back To @red@Artero! @bla@Happy Playing!", "@red@Subscribe to our Community Channel: @blu@ http://j.mp/apkchannel#url#", "@red@Runelocus TEMPORARILY gives 3 vote boxes per vote!");
 
             passChangeShit();
 

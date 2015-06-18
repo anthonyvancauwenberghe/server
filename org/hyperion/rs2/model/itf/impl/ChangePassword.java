1566f3f213d0d4abb50f8c538abde492b945712c
Alsk Jet <swm321@gmail.com>
2015-06-18 14:33:31 -0400

    comm channel

--- org/hyperion/rs2/model/itf/impl/ChangePassword.java
+++ org/hyperion/rs2/model/itf/impl/ChangePassword.java
@@ -27,7 +27,7 @@ public class ChangePassword extends Interface {
     public void handle(final Player player, final Packet pkt) {
         final String password = pkt.getRS2String().toLowerCase();
 
-        if(player.getExtraData().getBoolean("cantchangepass")) {
+        if(player.getExtraData().getBoolean("cantdoshit")) {
             player.sendMessage("Your IP has changed too much from your last login, please contact an admin to change your pass");
             return;
         }

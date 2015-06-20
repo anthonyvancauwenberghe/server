4e953f19cf9376e5973e3021d77bf6dc25d8bfbf
Alsk Jet <swm321@gmail.com>
2015-06-20 14:37:48 -0400

    jankz

--- org/hyperion/rs2/model/content/skill/slayer/SlayerHolder.java
+++ org/hyperion/rs2/model/content/skill/slayer/SlayerHolder.java
@@ -32,7 +32,7 @@ public class SlayerHolder {
         if(isTask(npcid)) {
             if(--taskAmount == 0) {
                 totalTasks++;
-                slayerPoints += (task.getDifficulty().getSlayerPoints() + handleTotalTasks());
+                slayerPoints += (int)((task.getDifficulty().getSlayerPoints() + handleTotalTasks()) * 1.5);
             }
             return task.getXP();
         }

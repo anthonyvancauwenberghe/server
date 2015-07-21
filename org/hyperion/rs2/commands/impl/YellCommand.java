cc5a56836c5d70981da62ec983ba0564201bb64e
Alsk Jet <swm321@gmail.com>
2015-07-20 23:38:49 -0400

    yell spam

--- org/hyperion/rs2/commands/impl/YellCommand.java
+++ org/hyperion/rs2/commands/impl/YellCommand.java
@@ -43,7 +43,7 @@ public class YellCommand extends Command {
 
 	public static final int NORMAL_YELL_DELAY = 150000;
 	public static final int DONATOR_YELL_DELAY = 120000;
-    public static final int SUPER_YELL_DELAY = 7000;
+    public static final int SUPER_YELL_DELAY = 90000;
 	public static final int SKILLER_YELL_DELAY = 90000;
 	
 	public static int minYellRank = 0;

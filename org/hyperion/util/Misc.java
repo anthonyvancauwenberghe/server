96c9e24c0f010569200985eede07f3b374942f87
Glis6 <gillesv@live.be>
2015-07-31 19:37:39 +0200

    Misc

--- org/hyperion/util/Misc.java
+++ org/hyperion/util/Misc.java
@@ -108,6 +108,22 @@ public class Misc {
 		return Math.round(value * power_of_ten) / power_of_ten;
 	}
 
+	public static String formatPlayerName(String str) {
+		str = ucFirst(str);
+		str.replace("_", " ");
+		return str;
+	}
+
+	public static String ucFirst(String str) {
+		str = str.toLowerCase();
+		if(str.length() > 1) {
+			str = str.substring(0,1).toUpperCase() + str.substring(1);
+		} else {
+			return str.toUpperCase();
+		}
+		return str;
+	}
+
 
 	public static String formatDocumentNumber(int value) {
 		char[] chars;

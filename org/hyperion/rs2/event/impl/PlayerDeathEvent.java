58c1bc71e8a0ff26bc621d6d4bce17de19479b46
Alsk Jet <swm321@gmail.com>
2015-08-30 11:17:28 -0400

    barrowsffa

--- org/hyperion/rs2/event/impl/PlayerDeathEvent.java
+++ org/hyperion/rs2/event/impl/PlayerDeathEvent.java
@@ -192,9 +192,7 @@ public class PlayerDeathEvent extends Event {
         if((player.duelAttackable > 0 || (killer != null && killer.duelAttackable > 0)) ||
 				(Duel.inDuelLocation(killer) || Duel.inDuelLocation(player)) || player.hasDuelTimer()) {    //If dying in duel arena
 			Duel.finishFullyDuel(player);
-        } else if (BarrowsFFA.barrowsFFA.deathCheck(player, killer == null ? player : killer)) {
-
-        } else if (player.getDungeoneering().inDungeon()) {
+        } if (player.getDungeoneering().inDungeon()) {
             DungeoneeringManager.handleDying(player);
         } else if(World.getWorld().getContentManager().handlePacket(6, player, ClickId.ATTACKABLE)) {
 			if(World.getWorld().getContentManager().handlePacket(6, player, ClickId.FIGHT_PITS_DEATH))

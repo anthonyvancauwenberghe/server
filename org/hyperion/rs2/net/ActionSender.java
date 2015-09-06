44aea18f897420848a2ae8b7a3efe12bb0cd7d4a
Alsk Jet <swm321@gmail.com>
2015-09-05 23:49:23 -0400

    2x vote off

--- org/hyperion/rs2/net/ActionSender.java
+++ org/hyperion/rs2/net/ActionSender.java
@@ -176,7 +176,6 @@ public class ActionSender {
 			player.sendMessage("@bla@Welcome back to @dre@ArteroPK@bla@.", "");
 			player.sendMessage("@bla@Run client in high-priority for fast speeds: @blu@ http://j.mp/fastclient#url#");
 			//Template for Bonus events: @dre@Bonus active: @bla@FILL IN BONUS HERE (2x has no capital x)
-			player.sendMessage("", "@dre@Bonus Active: @bla@ 2x vote reward!");
 			passChangeShit();
 
 

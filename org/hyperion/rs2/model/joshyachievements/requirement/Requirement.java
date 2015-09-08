4032441cdd88554b8d49967f2ad546ecf9e4f262
func__@hotmail.com <func__@hotmail.com>
2015-09-07 22:21:44 -0400

    achievements!!!!!!

--- org/hyperion/rs2/model/joshyachievements/requirement/Requirement.java
+++
@@ -1,7 +0,0 @@
-package org.hyperion.rs2.model.joshyachievements.requirement;
-
-import java.util.function.Supplier;
-
-public interface Requirement extends Supplier<Integer>{
-
-}
---
+++ org/hyperion/rs2/model/joshyachievementsv2/tracker/AchievementProgress.java
@@ -0,0 +1,82 @@
+package org.hyperion.rs2.model.joshyachievementsv2.tracker;
+
+import java.util.Date;
+import java.util.Map;
+import java.util.TreeMap;
+import org.hyperion.rs2.model.Player;
+import org.hyperion.rs2.model.joshyachievementsv2.Achievement;
+import org.hyperion.rs2.model.joshyachievementsv2.Achievements;
+
+public class AchievementProgress{
+
+    public final int achievementId;
+    private final Map<Integer, AchievementTaskProgress> progress;
+
+    public Date start;
+    public Date finish;
+
+    public AchievementProgress(final int achievementId){
+        this.achievementId = achievementId;
+
+        progress = new TreeMap<>();
+
+        achievement().tasks.stream()
+                .forEach(t -> add(new AchievementTaskProgress(achievementId, t.id)));
+    }
+
+    public void startNow(){
+        start = new Date();
+    }
+
+    public void finishNow(){
+        finish = new Date();
+    }
+
+    public Achievement achievement(){
+        return Achievements.get().get(achievementId);
+    }
+
+    public void add(final AchievementTaskProgress atp){
+        progress.put(atp.taskId, atp);
+    }
+
+    public AchievementTaskProgress progress(final int taskId){
+        return progress.get(taskId);
+    }
+
+    public int progress(){
+        return progress.values().stream()
+                .mapToInt(atp -> atp.progress)
+                .sum();
+    }
+
+    public boolean started(){
+        return start != null;
+    }
+
+    public boolean finished(){
+        return start != null
+                && finish != null
+                && finish.after(start)
+                && tasksFinished();
+    }
+
+    public boolean tasksFinished(){
+        return progress.values().stream()
+                    .allMatch(AchievementTaskProgress::finished);
+    }
+
+    public void sendProgressHeader(final Player player){
+        player.sendf("[@blu@%s@bla@] @blu@%,d@bla@ / @red@%,d@bla@ | @gre@%1.2f%%@bla@ Complete",
+                achievement().title, progress(), achievement().tasks.threshold, progressPercent());
+    }
+
+    public void sendAllTaskProgressHeaders(final Player player){
+        progress.values()
+                .forEach(p -> p.sendProgress(player, false));
+    }
+
+    public double progressPercent(){
+        return (double)progress() / achievement().tasks.threshold;
+    }
+}

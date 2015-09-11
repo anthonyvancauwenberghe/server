da4ec888dce4a056cc8adb5dc57a5312398334f2
Glis <gillesv@live.be>
2015-09-11 22:28:18 +0200

    Fletching cleanup & login message

--- org/hyperion/rs2/model/content/skill/fletching/Fletching.java
+++ org/hyperion/rs2/model/content/skill/fletching/Fletching.java
@@ -8,9 +8,13 @@ import org.hyperion.rs2.model.World;
 import org.hyperion.rs2.model.content.ContentEntity;
 import org.hyperion.rs2.model.content.ContentTemplate;
 import org.hyperion.rs2.model.content.skill.crafting.Crafting;
+import org.hyperion.rs2.model.content.skill.crafting.GemCutting;
 import org.hyperion.rs2.model.content.skill.crafting.LeatherCrafting;
+import org.hyperion.util.ArrayUtils;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Fletching skill handler
@@ -22,46 +26,6 @@ public class Fletching implements ContentTemplate {
 	public static int EXPMULTIPLIER = Constants.XPRATE * 10;
 
 	public Fletching() {
-	}
-
-	public static final int[] ARROWS = {882, 884, 886, 888, 890, 892};
-	public static final int[] ARROW_HEADS = {39, 40, 41, 42, 43, 44};
-	public static final int[] ARROW_LEVELS = {1, 15, 30, 45, 60, 75};
-	public static final int[] ARROW_EXPERIENCE = {40, 57, 95, 133, 168, 207};
-
-	public static final int[] DARTS = {806, 807, 808, 809, 810, 811};
-	public static final int[] DART_TIPS = {819, 820, 821, 822, 823, 824};
-	public static final int[] DART_LEVELS = 	{1, 22, 37, 52, 67, 81};
-	public static final int[] DART_EXPERIENCE = {2, 4, 	8, 	12, 15, 19, 25};
-
-	public static int getIntArray(int[] array1, int[] array2, int bow) {
-		int a = 0;
-		for(int object : array1) {
-			if(object == bow) {
-				return array2[a];
-			}
-			a++;
-		}
-		return - 1;
-	}
-
-	public String getStringArray(int[] array1, String[] array2, int bow) {
-		int a = 0;
-		for(int object : array1) {
-			if(object == bow) {
-				return array2[a];
-			}
-			a++;
-		}
-		return "";
-	}
-
-	public int contains(int[] array, int value) {
-		for(int i : array) {
-			if(i == value)
-				return value;
-		}
-		return 0;
 	}
 
 	public boolean isFletchable(Player client, int slot1, int slot2, int useItem, int usedItem) {
@@ -119,15 +83,28 @@ public class Fletching implements ContentTemplate {
 		return false;
 	}
 
+	private int[] fletchingTools = {314, 53};
+
 	@Override
 	public int[] getValues(int type) {
 		if (type == 13) {
-			int ai[] = {
-					946, 1777, 314, 50, 48, 54, 56, 58, 60, 62,
-					64, 66, 68, 70, 72, 39, 40, 41, 42, 43,
-					44, 806, 807, 808, 809, 810, 811, 53
-			};
-			return ai;
+			List<Integer> j = new ArrayList();
+			for(ArrowMaking.Arrow arrow : ArrowMaking.Arrow.values()) {
+				j.add(arrow.getArrowHeadId());
+			}
+			for(BowStringing.StringItem string : BowStringing.StringItem.values()) {
+				j.add(string.getItemId());
+			}
+			for(BowStringing.StrungItems item : BowStringing.StrungItems.values()) {
+				j.add(item.getItemId());
+			}
+			for(DartMaking.Dart dart : DartMaking.Dart.values()) {
+				j.add(dart.getDartTipId());
+			}
+			for(int i = 0; i < fletchingTools.length; i++) {
+				j.add(fletchingTools[i]);
+			}
+			return ArrayUtils.fromList(j);
 		}
 		return null;
 	}
@@ -222,58 +199,6 @@ public class Fletching implements ContentTemplate {
 		if(type == 13) {
 			return isFletchable(player, slot, itemSlot2, id, itemId2);
 		}
-		if(type == 0) {
-			if(player.getExtraData().getBoolean("crafting")) {
-				return Crafting.clickInterface(player, id);
-			}
-		}
 		return false;
 	}
-
-	@Override
-	public void init() throws FileNotFoundException {
-	}
-
-	
-	
-	/*
-	 *work in progress
-	 */
-
-	public enum Enchanted_Bolts {
-
-		Bronze_Arrow(new int[]{39, 53}, 882, 1, 40),
-		Iron_Arrow(new int[]{40, 53}, 884, 15, 57),
-		Steel_Arrow(new int[]{41, 53}, 886, 30, 95),
-		Mith_Arrow(new int[]{42, 53}, 888, 45, 132),
-		Addy_Arrow(new int[]{43, 53}, 890, 60, 165),
-		Rune_Arrow(new int[]{44, 53}, 892, 75, 208),
-		Dragon_Arrow(new int[]{11237, 53}, 11212, 75, 255),
-
-		Iron_Bolt(new int[]{9377, 314}, 9140, 39, 2),
-		Steel_Bolt(new int[]{9378, 314}, 9141, 46, 3),
-		Mith_Bolt(new int[]{9379, 314}, 9142, 53, 5),
-		Addy_Bolt(new int[]{9380, 314}, 9143, 61, 7),
-		Rune_Bolt(new int[]{9381, 314}, 9144, 69, 10),
-
-		Sapphire_Bolt(new int[]{9142, 9189}, 9240, 56, 5),
-		Emerald_Bolt(new int[]{9142, 9190}, 9241, 58, 6),
-		Ruby_Bolt(new int[]{9143, 9191}, 9242, 63, 7),
-		Diamond_Bolt(new int[]{9143, 9192}, 9243, 65, 8),
-		Dragon_Bolt(new int[]{9144, 9193}, 9244, 71, 9),
-		Onyx_Bolt(new int[]{9144, 9194}, 9245, 73, 10);
-
-		Enchanted_Bolts(int[] items, int finishId, int level, int xp) {
-			this.items = items;
-			this.finishId = finishId;
-			this.level = level;
-			this.xp = xp;
-		}
-
-		int[] items;
-		int finishId;
-		int level;
-		int xp;
-	}
-
 }

3d735528b2e727adac49d30abc80b3a66b67671a
func__@hotmail.com <func__@hotmail.com>
2015-09-21 22:41:06 -0400

    necessary achievements fix

--- org/hyperion/rs2/model/joshyachievementsv2/tracker/AchievementTaskProgress.java
+++ org/hyperion/rs2/model/joshyachievementsv2/tracker/AchievementTaskProgress.java
@@ -29,7 +29,10 @@ public class AchievementTaskProgress{
     }
 
     public int progress(final int amount){
-        return progress = Math.max(progress + amount, task().threshold);
+        progress += amount;
+        if(progress > task().threshold)
+            progress = task().threshold;
+        return progress;
     }
 
     public double progressPercent(){

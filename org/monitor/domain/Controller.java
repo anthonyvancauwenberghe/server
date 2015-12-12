c0d1e2364d47de924ff9a3e7bbac1b2393756512
Glis <gillesv@live.be>
2015-12-12 19:40:23 +0100

    Disable

--- org/monitor/domain/Controller.java
+++ org/monitor/domain/Controller.java
@@ -6,7 +6,6 @@ import javafx.stage.Stage;
 import javafx.stage.WindowEvent;
 import org.hyperion.Server;
 import org.hyperion.rs2.model.World;
-import org.monitor.gui.SelectionTabController;
 
 /**
  * Created by Gilles on 24/11/2015.
@@ -18,7 +17,7 @@ public class Controller extends Application {
     @Override
     public void start(Stage stage) {
         gameWorld = Server.launchServer();
-        scene = new Scene(new SelectionTabController(gameWorld));
+        //scene = new Scene(new SelectionTabController(gameWorld));
         stage.setScene(scene);
 
         stage.setOnShown((WindowEvent t) -> {

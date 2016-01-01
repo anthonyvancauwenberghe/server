23ea6de9f722e5cff221dceb489b254723e3ef45
deviouspk <aamining2@gmail.com>
2016-01-01 13:48:11 +0100

    Revert "...."
    This reverts commit 4cd9b37510ecf479898ae95bedf565b8a4949d8f.

--- org/hyperion/rs2/sql/event/impl/RichWhitelistEvent.java
+++ org/hyperion/rs2/sql/event/impl/RichWhitelistEvent.java
@@ -3,7 +3,6 @@ package org.hyperion.rs2.sql.event.impl;
 import org.hyperion.rs2.commands.Command;
 import org.hyperion.rs2.commands.CommandHandler;
 import org.hyperion.rs2.model.Player;
-import org.hyperion.rs2.model.Rank;
 import org.hyperion.rs2.sql.SQLConnection;
 import org.hyperion.rs2.sql.event.SQLEvent;
 import org.hyperion.rs2.sql.requests.RichWhitelistRequest;
@@ -34,7 +33,7 @@ public class RichWhitelistEvent extends SQLEvent {
     }
 
     static {
-        CommandHandler.submit(new Command("disablerichevent", Rank.DEVELOPER) {
+        CommandHandler.submit(new Command("disablerichevent") {
             @Override
             public boolean execute(Player player, String input) throws Exception {
                 enabled = false;
@@ -42,7 +41,7 @@ public class RichWhitelistEvent extends SQLEvent {
                 return true;
             }
         });
-        CommandHandler.submit(new Command("enablerichevent", Rank.DEVELOPER) {
+        CommandHandler.submit(new Command("enablerichevent") {
             @Override
             public boolean execute(Player player, String input) throws Exception {
                 enabled = true;

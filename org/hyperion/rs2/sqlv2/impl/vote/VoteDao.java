88389f50c30326c06f1c1f186a1836e4a3bb782b
Glis <gillesv@live.be>
2016-02-05 16:19:24 +0100

    Update

--- org/hyperion/rs2/sqlv2/impl/vote/VoteDao.java
+++ org/hyperion/rs2/sqlv2/impl/vote/VoteDao.java
@@ -5,29 +5,28 @@ import org.skife.jdbi.v2.sqlobject.Bind;
 import org.skife.jdbi.v2.sqlobject.SqlQuery;
 import org.skife.jdbi.v2.sqlobject.SqlUpdate;
 import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
-import org.skife.jdbi.v2.sqlobject.mixins.Transactional;
 
 import java.util.List;
 
 @RegisterMapper(WaitingVoteMapper.class)
-public interface VoteDao extends SqlDao, Transactional<VoteDao> {
+public interface VoteDao extends SqlDao {
 
     @SqlQuery("SELECT * FROM waitingVotes WHERE realUsername = :playerName")
     List<WaitingVote> waiting(@Bind("playerName") final String playerName);
 
-    @SqlUpdate("UPDATE waitingVotes SET runelocusProcessed = TRUE WHERE index = :index")
+    @SqlUpdate("UPDATE waitingVotes SET runelocusProcessed = 1 WHERE 'index' = :index")
     int processRunelocus(@Bind("index") final int index);
 
-    @SqlUpdate("UPDATE waitingVotes SET topgProcessed = TRUE WHERE index = :index")
+    @SqlUpdate("UPDATE waitingVotes SET topgProcessed = 1 WHERE 'index' = :index")
     int processTopg(@Bind("index") final int index);
 
-    @SqlUpdate("UPDATE waitingVotes SET rspslistProcessed = TRUE WHERE index = :index")
+    @SqlUpdate("UPDATE waitingVotes SET rspslistProcessed = 1 WHERE 'index' = :index")
     int processRspslist(@Bind("index") final int index);
 
-    @SqlUpdate("UPDATE waitingVotes SET processed = TRUE WHERE index = :index")
+    @SqlUpdate("UPDATE waitingVotes SET processed = 1 WHERE 'index' = :index")
     int process(@Bind("index") final int index);
 
-    @SqlUpdate("DELETE FROM waitingVotes WHERE index = :index")
+    @SqlUpdate("DELETE FROM waitingVotes WHERE 'index' = :index")
     int delete(@Bind("index") final int index);
 
     @SqlUpdate("INSERT INTO votes (name, runelocus, top100, topg) VALUES (:name, :runelocus, :top100, :topg) ON DUPLICATE KEY UPDATE runelocus = runelocus + :runelocus, top100 = top100 + :top100, topg = topg + :topg")

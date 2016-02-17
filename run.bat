c4b6b8437cb805974134b8c91e6f35045e746362
arthur <aamining2@gmail.com>
2016-02-17 17:51:08 +0100

    remove news

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 title ArteroPk
 :her5
-java -Xmx5900m -cp bin;libs/jdbi-2.70.jar;libs/sqlite-jdbc-3.7.2.jar;libs/worldmap2010.jar;libs/mysql-connector.jar;libs/highscores.jar;libs/jython.jar;libs/xstream.jar;libs/xpp.jar;libs/slf4j-api-1.5.8.jar;libs/mina-core-2.0.0-M6.jar;libs/slf4j-jdk14-1.5.8.jar;libs/commons-compress-1.0.jar;libs/junit-4.6.jar org.hyperion.Server local
+java -Xmx5900m -cp bin;libs/gson-2.3.1;libs/jdbi-2.70.jar;libs/sqlite-jdbc-3.7.2.jar;libs/worldmap2010.jar;libs/mysql-connector.jar;libs/highscores.jar;libs/jython.jar;libs/xstream.jar;libs/xpp.jar;libs/slf4j-api-1.5.8.jar;libs/mina-core-2.0.0-M6.jar;libs/slf4j-jdk14-1.5.8.jar;libs/commons-compress-1.0.jar;libs/junit-4.6.jar;libs/commons-codec-1.10.jar org.hyperion.Server local
 pause

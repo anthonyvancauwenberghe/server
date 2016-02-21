28d854041ada95750a093aefc235cfa96a5725d6
Glis <gillesv@live.be>
2016-02-21 16:16:36 +0100

    sync

--- org/hyperion/map/pathfinding/PathTest.java
+++ org/hyperion/map/pathfinding/PathTest.java
@@ -11,18 +11,36 @@ public class PathTest {
 	/**
 	 * The map on which the units will move
 	 */
-	private static GameMap map = new GameMap();
+	private GameMap map = new GameMap();
 	/**
 	 * The path finder we'll use to search our map
 	 */
-	private static PathFinder finder = new AStarPathFinder(map, 32, true);
+	private PathFinder finder;
+	/**
+	 * The last path found for the current unit
+	 */
+	private Path path;
 
-	public static int baseX = 0;
-	public static int baseY = 0;
+	private static PathTest tester;
+
+	/**
+	 * Create a new test game for the path finding tutorial
+	 */
+	public PathTest() {
+		finder = new AStarPathFinder(map, 32, true);
+		tester = this;
+	}
+
+	public int baseX = 0;
+	public int baseY = 0;
+
+	public static PathTest getSingleton() {
+		return tester;
+	}
 
 	public final static int maxRegionSize = 25;//*2 in reality
 
-	public static Path getPath(int x, int y, int toX, int toY) {
+	public Path getPath(int x, int y, int toX, int toY) {
 		try {
 			baseX = x - maxRegionSize;
 			baseY = y - maxRegionSize;
@@ -31,18 +49,12 @@ public class PathTest {
 			if(toX < 0 || toX > (maxRegionSize * 2) || toY < 0 || toY > (maxRegionSize * 2)) {
 				return null;
 			}
-
-			try {
-				return finder.findPath(maxRegionSize, maxRegionSize, toX, toY);
-			} catch(Exception e) {
-				e.printStackTrace();
-				return null;
-			}
+			path = finder.findPath(maxRegionSize, maxRegionSize, toX, toY);
+			return path;
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 		return null;
 	}
-
 }
 

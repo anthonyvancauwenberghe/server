e16cdfaf1d2e6f989124fea010768e8fbf4a3ebe
Glis <gillesv@live.be>
2016-02-23 22:11:02 +0100

    Update task

--- org/hyperion/engine/Update.java
+++ org/hyperion/engine/Update.java
@@ -2,16 +2,17 @@ package org.hyperion.engine;
 
 import org.hyperion.Server;
 import org.hyperion.engine.task.Task;
+import org.hyperion.rs2.model.Player;
 import org.hyperion.rs2.model.World;
 import org.hyperion.rs2.model.container.Trade;
 import org.hyperion.rs2.model.content.clan.ClanManager;
-import org.hyperion.rs2.model.content.skill.dungoneering.Dungeon;
 import org.hyperion.rs2.savingnew.PlayerSaving;
 import org.hyperion.util.Time;
 
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 import java.util.logging.Level;
+import java.util.stream.Stream;
 
 /**
  * Created by Gilles on 11/02/2016.
@@ -22,7 +23,7 @@ public class Update extends Task {
     private String reason;
 
     public Update(int updateTimer, String reason) {
-        super(Time.ONE_SECOND, "update task");
+        super(Time.ONE_SECOND, "Update");
         this.updateTimer = updateTimer;
         this.reason = reason;
         World.getPlayers().stream().filter(player -> player != null).forEach(player -> player.getActionSender().sendUpdate(updateTimer));
@@ -32,22 +33,21 @@ public class Update extends Task {
     protected void execute() {
         try {
             if (!Server.isUpdating() || updateTimer < 0) {
-                Thread.interrupted();
+                stop();
                 return;
             }
             if ((updateTimer % 5 == 0 || updateTimer <= 10) && updateTimer != 0)
                 System.out.println("Time left before update: " + updateTimer);
             updateTimer--;
             if (updateTimer == 0) {
-                World.getPlayers().forEach(Trade::declineTrade);
-                Dungeon.activeDungeons.forEach(Dungeon::complete);
-                World.getPlayers().stream().filter(player -> player != null).forEach(PlayerSaving::save);
+                Stream<Player> playerStream = World.getPlayers().stream().filter(player -> player != null);
+                playerStream.forEach(Trade::declineTrade);
+                playerStream.forEach(PlayerSaving::save);
                 ClanManager.save();
                 Server.getLogger().info("Update task finished! Reason for update: " + reason);
                 try(ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("./data/key.dat"))) {
                     out.writeObject(Server.getCharFileEncryption().getKey());
                 }
-                stop();
                 Runtime.getRuntime().exec("cmd /c start run.bat");
                 System.exit(1);
             }
